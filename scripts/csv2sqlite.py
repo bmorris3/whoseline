657b5b301dd78f9055e80f812accb0c6a47509a1
Brett Morris <brettmorris21@gmail.com>
2017-11-16 15:22:05 +0200

    PEP8 fixes

--- scripts/csv2sqlite.py
+++ scripts/csv2sqlite.py
@@ -43,10 +43,12 @@ def import_file(csv_file, args):
     
     if not result:
         print("Adding source %s..." % args.source)
-        c.execute('''INSERT INTO sources (display_name, short_name, url, description) VALUES (?, ?, ?, ?)''', (args.display_name, args.source, args.url, args.description))
+        c.execute('''INSERT INTO sources (display_name, short_name, url, description) VALUES (?, ?, ?, ?)''',
+                  (args.display_name, args.source, args.url, args.description))
         conn.commit()
         
-        c.execute('''SELECT * FROM sources WHERE short_name = ? ''', (args.source,))
+        c.execute('''SELECT * FROM sources WHERE short_name = ? ''',
+                  (args.source,))
         result = c.fetchone()
     
     source_id, _, _, _, _ = result
@@ -54,7 +56,8 @@ def import_file(csv_file, args):
     headers = args.headers.split(',')
     
     if len(headers) != 3:
-        sys.exit("Incorrect number of headers or columns specified. Three are required.")
+        sys.exit("Incorrect number of headers or columns specified. "
+                 "Three are required.")
     
     with open(csv_file) as f:
         try:
@@ -63,14 +66,17 @@ def import_file(csv_file, args):
             reader = csv.reader(f, delimiter=args.separator)
             
         except ValueError:
-            print("Could not convert %r to integer column indices; assuming text headers" % headers)
+            print("Could not convert %r to integer column indices; assuming "
+                  "text headers" % headers)
             wavelength, species, priority = headers
             reader = csv.DictReader(f, delimiter=args.separator)
             
         added = 0
         
         for row in reader:
-            c.execute('''INSERT INTO data (source_id, wavelength, species, priority) VALUES (?, ?, ?, ?)''', (source_id, float(row[wavelength]), row[species], float(row[priority])))
+            c.execute('''INSERT INTO data (source_id, wavelength, species, priority) VALUES (?, ?, ?, ?)''',
+                      (source_id, float(row[wavelength]), row[species],
+                       float(row[priority])))
             added += 1
     
     conn.commit()
@@ -80,18 +86,31 @@ def import_file(csv_file, args):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Import a CSV data file into the sqlite database.')
+    parser = argparse.ArgumentParser(description='Import a CSV data file into '
+                                                 'the sqlite database.')
     parser.add_argument('database', help='The full path to the sqlite database')
     parser.add_argument('source', help='The short name of the source')
     
-    parser.add_argument('files', nargs='+', help='Files to be imported, Multiple files can be imported at once if they share the same delimiter and metadata.')
+    parser.add_argument('files', nargs='+',
+                        help='Files to be imported, Multiple files can be '
+                             'imported at once if they share the same delimiter'
+                             ' and metadata.')
     
-    parser.add_argument('--separator', '-s', help='The field separator used by the file(s)', default=str(u","))
-    parser.add_argument('--headers', '-H', help='comma-separated list of the wavelength, species and priority header names, in that order. If there are no headers in the file, the corresponding column numbers should be given instead (starting from 0).', default='0,1,2')
+    parser.add_argument('--separator', '-s',
+                        help='The field separator used by the file(s)',
+                        default=str(u","))
+    parser.add_argument('--headers', '-H',
+                        help='comma-separated list of the wavelength, species '
+                             'and priority header names, in that order. If '
+                             'there are no headers in the file, the '
+                             'corresponding column numbers should be given '
+                             'instead (starting from 0).', default='0,1,2')
 
-    parser.add_argument('--display-name', '-n', help='The display name of the source')
+    parser.add_argument('--display-name', '-n',
+                        help='The display name of the source')
     parser.add_argument('--url', '-u', help='The url of the source')
-    parser.add_argument('--description', '-d', help='The description of the source')
+    parser.add_argument('--description', '-d',
+                        help='The description of the source')
     args = parser.parse_args()
     
     if not os.path.exists(args.database):
@@ -99,5 +118,3 @@ if __name__ == '__main__':
         
     for csv_file in args.files:
         import_file(csv_file, args)
-    
-        
